8d2ae99fa931126eb31293abb4b9108fab062f09
Alberto Navas <albertonavasang@gmail.com>
2023-12-27 20:38:26 +0100

    Changed "clk_signal" name to "sck_signal"

--- Modules/spi_master/hdl/spi_master.vhd
+++ Modules/spi_master/hdl/spi_master.vhd
@@ -51,7 +51,7 @@ end spi_master;
 architecture Behavioral of spi_master is
     
     -- Signals
-    signal clk_signal : std_logic := '0';
+    signal sck_signal : std_logic := '0';
     signal done_signal: std_logic := '0';
     signal bit_counter_signal : integer range 0 to 7 := 0;
     signal shift_data_signal : std_logic_vector(7 downto 0) := "00000000";
@@ -85,21 +85,21 @@ begin
     tx_proc: process(clk, reset)
     begin
         if (reset = '1') then
-            clk_signal <= '1';
+            sck_signal <= '1';
             bit_counter_signal <= 0;
             shift_data_signal <= "00000000";
             done_signal <= '0';
         elsif (rising_edge(clk)) then
             if (state = s_busy) then
-                if (clk_signal = '1') then
-                    clk_signal <= '0';
+                if (sck_signal = '1') then
+                    sck_signal <= '0';
                     if (bit_counter_signal = 0) then
                         shift_data_signal <= data;
                     else
                         shift_data_signal <= shift_data_signal(6 downto 0) & '0';
                     end if;
                 else
-                    clk_signal <= '1';
+                    sck_signal <= '1';
                     if (bit_counter_signal < 7) then
                         bit_counter_signal <= bit_counter_signal + 1;
                     else
@@ -107,7 +107,7 @@ begin
                     end if;
                 end if;
              else
-                clk_signal <= '1';
+                sck_signal <= '1';
                 bit_counter_signal <= 0;
                 shift_data_signal <= "00000000";
                 done_signal <= '0';
@@ -117,7 +117,7 @@ begin
     
     mosi <= shift_data_signal(7);
     cs   <= '0' when (state = s_busy) else '1';
-    sck  <= clk_signal when (state = s_busy) else '1';
+    sck  <= sck_signal when (state = s_busy) else '1';
     
     -- Debug signals
     bit_counter <= std_logic_vector(to_unsigned(bit_counter_signal, 4));
