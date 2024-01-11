aa421999c88dbb3a012021ca830c9b498bb71f11
Alberto Navas <albertonavasang@gmail.com>
2024-01-11 22:20:26 +0100

    Rearranged indents and modified testbench
    Rearranged all lines using "Beautify Buffer" from "VHDL Mode" plugin in Sublime Text,
    Add more cases to testbench to show correct behaviour

--- Modules/spi_master/hdl/spi_master.vhd
+++ Modules/spi_master/hdl/spi_master.vhd
@@ -25,49 +25,49 @@ use IEEE.NUMERIC_STD.ALL;
 
 
 entity spi_master is
-    Port ( 
-          -- Basic
-          clk   : in std_logic;
-          reset : in std_logic;
-          
-          -- Control
-          start : in  std_logic;
-          ready : out std_logic;
-          
-          -- SPI
-          mosi         : out std_logic;
-          sck          : out std_logic;
-          cs           : out std_logic;
-          
-          -- Data
-          data : in std_logic_vector(7 downto 0);
-          
-          -- Debug 
-          done_dbg        : out std_logic;
-          bit_counter_dbg : out std_logic_vector(2 downto 0);
-          shift_data_dbg  : out std_logic_vector(7 downto 0);
-          start_delay_signal_dbg : out std_logic;
-          start_rising_edge_flag_dbg : out std_logic
-        );
+    Port (
+        -- Basic
+        clk   : in std_logic;
+        reset : in std_logic;
+
+        -- Control
+        start : in  std_logic;
+        ready : out std_logic;
+
+        -- SPI
+        mosi : out std_logic;
+        sck  : out std_logic;
+        cs   : out std_logic;
+
+        -- Data
+        data : in std_logic_vector(7 downto 0);
+
+        -- Debug 
+        done_dbg                   : out std_logic;
+        bit_counter_dbg            : out std_logic_vector(2 downto 0);
+        shift_data_dbg             : out std_logic_vector(7 downto 0);
+        start_delay_signal_dbg     : out std_logic;
+        start_rising_edge_flag_dbg : out std_logic
+    );
 end spi_master;
 
 architecture Behavioral of spi_master is
-    
+
     -- Signals
-    signal start_delay_signal : std_logic := '0';
-    signal start_rising_edge_flag : std_logic := '0';
-    signal sck_signal : std_logic := '0';
-    signal done_signal: std_logic := '0';
-    signal bit_counter_signal : unsigned (2 downto 0) := "000";
-    signal shift_data_signal : std_logic_vector(7 downto 0) := "00000000";
-    
+    signal start_delay_signal     : std_logic                    := '0';
+    signal start_rising_edge_flag : std_logic                    := '0';
+    signal sck_signal             : std_logic                    := '0';
+    signal done_signal            : std_logic                    := '0';
+    signal bit_counter_signal     : unsigned (2 downto 0)        := "000";
+    signal shift_data_signal      : std_logic_vector(7 downto 0) := "00000000";
+
     -- FSM
     type state_t is (s_ready, s_busy);
     signal state : state_t := s_ready;
-    
+
 begin
-    
-    start_rise_proc: process(clk, reset)
+
+    start_rise_proc : process(clk, reset)
     begin
         if (reset = '1') then
             start_delay_signal     <= '0';
@@ -81,9 +81,9 @@ begin
             start_delay_signal <= start;
         end if;
     end process;
-    
-   
-    FSM_proc: process(clk, reset)
+
+
+    FSM_proc : process(clk, reset)
     begin
         if (reset = '1') then
             state <= s_ready;
@@ -100,10 +100,10 @@ begin
             end case;
         end if;
     end process;
-    
+
     ready <= '1' when (state = s_ready) else '0';
-    
-    tx_proc: process(clk, reset)
+
+    tx_proc : process(clk, reset)
     begin
         if (reset = '1') then
             sck_signal         <= '1';
@@ -131,7 +131,7 @@ begin
                         done_signal <= '1';
                     end if;
                 end if;
-             else
+            else
                 sck_signal         <= '1';
                 bit_counter_signal <= "000";
                 shift_data_signal  <= "00000000";
@@ -139,16 +139,16 @@ begin
             end if;
         end if;
     end process;
-    
+
     mosi <= shift_data_signal(7) when (state = s_busy) else '0';
-    cs   <= '0' when (state = s_busy) else '1';
-    sck  <= sck_signal when (state = s_busy) else '1';
-    
+    cs   <= '0'                  when (state = s_busy) else '1';
+    sck  <= sck_signal           when (state = s_busy) else '1';
+
     -- Debug signals
-    done_dbg        <= done_signal;
-    bit_counter_dbg <= std_logic_vector(bit_counter_signal);
-    shift_data_dbg  <= shift_data_signal;
-    start_delay_signal_dbg <= start_delay_signal;
+    done_dbg                   <= done_signal;
+    bit_counter_dbg            <= std_logic_vector(bit_counter_signal);
+    shift_data_dbg             <= shift_data_signal;
+    start_delay_signal_dbg     <= start_delay_signal;
     start_rising_edge_flag_dbg <= start_rising_edge_flag;
-    
+
 end Behavioral;
--- Modules/spi_master/tb/spi_master_tb.vhd
+++ Modules/spi_master/tb/spi_master_tb.vhd
@@ -37,109 +37,107 @@ end spi_master_tb;
 
 architecture Behavioral of spi_master_tb is
 
--- Instantiate the COmponent Under Test
-component spi_master is
-  Port ( 
-         -- Basic
-         clk   : in std_logic;
-         reset : in std_logic;
-         
-         -- Control
-         start : in  std_logic;
-         ready : out std_logic;
-         
-         -- SPI
-         mosi         : out std_logic;
-         sck          : out std_logic;
-         cs           : out std_logic;
-         
-         -- Data
-         data : in std_logic_vector(7 downto 0);
-         
-         -- Debug
-         done_dbg        : out std_logic;
-         bit_counter_dbg : out std_logic_vector(2 downto 0);
-         shift_data_dbg  : out std_logic_vector(7 downto 0);
-         start_delay_signal_dbg : out std_logic;
-         start_rising_edge_flag_dbg : out std_logic
+    -- Instantiate the COmponent Under Test
+    component spi_master is
+        Port (
+            -- Basic
+            clk   : in std_logic;
+            reset : in std_logic;
+
+            -- Control
+            start : in  std_logic;
+            ready : out std_logic;
+
+            -- SPI
+            mosi : out std_logic;
+            sck  : out std_logic;
+            cs   : out std_logic;
+
+            -- Data
+            data : in std_logic_vector(7 downto 0);
+
+            -- Debug
+            done_dbg                   : out std_logic;
+            bit_counter_dbg            : out std_logic_vector(2 downto 0);
+            shift_data_dbg             : out std_logic_vector(7 downto 0);
+            start_delay_signal_dbg     : out std_logic;
+            start_rising_edge_flag_dbg : out std_logic
         );
-end component;
-    
+    end component;
+
     -- Clock
     constant clk_period : time := 1 ns;
-    
+
     -- Signals
     signal clk, reset, start, ready, mosi, sck, cs : std_logic;
-    signal data : std_logic_vector(7 downto 0);
-    
+    signal data                                    : std_logic_vector(7 downto 0);
+
     signal done_dbg                   : std_logic;
     signal bit_counter_dbg            : std_logic_vector(2 downto 0);
     signal shift_data_dbg             : std_logic_vector(7 downto 0);
     signal start_delay_signal_dbg     : std_logic;
     signal start_rising_edge_flag_dbg : std_logic;
-    
+
 begin
-    
+
     -- Port Mapping
     UUT: spi_master
-        Port Map( clk   => clk,
-                  reset => reset,
-                  
-                  -- Control
-                  start => start,
-                  ready => ready,
-                  
-                  -- SPI
-                  mosi         => mosi,
-                  sck          => sck,
-                  cs           => cs,
-                  
-                  -- Data
-                  data => data,
-                  
-                  -- Debug
-                  done_dbg        => done_dbg,
-                  bit_counter_dbg => bit_counter_dbg,
-                  shift_data_dbg  => shift_data_dbg,
-                  start_delay_signal_dbg => start_delay_signal_dbg,
-                  start_rising_edge_flag_dbg => start_rising_edge_flag_dbg
-                );
-                
+        Port Map ( 
+            clk => clk,
+            reset => reset,
+
+            -- Control
+            start => start,
+            ready => ready,
+
+            -- SPI
+            mosi => mosi,
+            sck  => sck,
+            cs   => cs,
+
+            -- Data
+            data => data,
+
+            -- Debug
+            done_dbg                   => done_dbg,
+            bit_counter_dbg            => bit_counter_dbg,
+            shift_data_dbg             => shift_data_dbg,
+            start_delay_signal_dbg     => start_delay_signal_dbg,
+            start_rising_edge_flag_dbg => start_rising_edge_flag_dbg
+        );
+
     -- Stimulus processes
-                  
-    clk_proc: process
+    clk_proc : process
     begin
         clk <= '0';
-            wait for clk_period/2;
+        wait for clk_period/2;
         clk <= '1';
-            wait for clk_period/2;
+        wait for clk_period/2;
     end process;
-    
-    stim_proc: process
+
+    stim_proc : process
     begin
-    reset     <= '1';
-    start     <= '0';
-    data      <= "00000000";
-    wait for 2*clk_period; 
-    reset     <= '0';
-    start     <= '0';
-    data      <= "10011001";
-    wait for 2*clk_period;
-    reset     <= '0';
-    start     <= '1';
-    wait for 2*clk_period;
-    reset     <= '0';
-    start     <= '0';
-    wait for 16*clk_period;
-    reset     <= '0';
-    start     <= '1';
-    data      <= "00110010";
-    wait for 1*clk_period;
-    reset     <= '0';
-    start     <= '0';
-    wait;
-    
-    
+        reset <= '1';
+        start <= '0';
+        data  <= "00000000";
+        wait for 2*clk_period;
+        reset <= '0';
+        start <= '0';
+        data  <= "10011001";
+        wait for 2*clk_period;
+        start <= '1';
+        wait for 3*clk_period;
+        start <= '0';
+        wait for 10*clk_period;
+        start <= '1';
+        data  <= "00110010";
+        wait for 1*clk_period;
+        start <= '0';
+        wait for 5*clk_period;
+        start <= '1';
+        wait for 1*clk_period;
+        start <= '0';
+        wait for 20*clk_period;
     end process;
-    
+
 end Behavioral;
