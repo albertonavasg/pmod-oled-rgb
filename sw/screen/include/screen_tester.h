56461a61ac50f287bb247c08441f69bc4b12fbd4
Alberto Navas <albertonavasang@gmail.com>
2025-12-19 19:24:36 +0100

    [SW] Expanded broadcastCommand to broadcast:
    - Modified broadcast method to allow for any Screen method

--- sw/screen/include/screen_tester.h
+++ sw/screen/include/screen_tester.h
@@ -1,8 +1,10 @@
 #ifndef SCREEN_TESTER_H
 #define SCREEN_TESTER_H
 
-#include <vector>  // vector
-#include <utility> // forward
+#include <vector>      // vector
+#include <utility>     // forward
+#include <functional>  // reference_wrapper, invoke
+#include <type_traits> // is_invocable_v
 
 #include "screen.h"
 
@@ -18,11 +20,13 @@ class ScreenTester {
 
         private:
             std::vector<std::reference_wrapper<Screen>> m_screens;
-            // Broadcast command to all screens
-            template <typename Rep, typename Period, typename... Args>
-            void broadcastCommand(const std::chrono::duration<Rep, Period>& delay = std::chrono::duration<Rep, Period>(0), Args&&... args) {
+
+            // Broadcast method to all screens
+            template <typename F, typename... Args>
+            void broadcast(F&& f, std::chrono::milliseconds delay = std::chrono::milliseconds(0), Args&&... args)
+            {
                 for (Screen& s : m_screens) {
-                    s.sendCommand(std::forward<Args>(args)...);
+                    std::invoke(std::forward<F>(f), s, std::forward<Args>(args)...);
                 }
                 if (delay.count() > 0) {
                     std::this_thread::sleep_for(delay);
--- sw/screen/src/screen_tester.cpp
+++ sw/screen/src/screen_tester.cpp
@@ -18,20 +18,17 @@ ScreenTester::ScreenTester(const std::vector<std::reference_wrapper<Screen>>& sc
 
 void ScreenTester::testDisplay() {
 
-    broadcastCommand(500ms, screen::Command::EntireDisplayOn);
-    broadcastCommand(500ms, screen::Command::EntireDisplayOff);
-    broadcastCommand(200ms, screen::Command::EntireDisplayOn);
-    broadcastCommand(200ms, screen::Command::NormalDisplay);
+    broadcast([](Screen& s){s.sendCommand(screen::Command::EntireDisplayOn);}, 500ms);
+    broadcast([](Screen& s){s.sendCommand(screen::Command::EntireDisplayOff);}, 500ms);
+    broadcast([](Screen& s){s.sendCommand(screen::Command::EntireDisplayOn);}, 500ms);
+    broadcast([](Screen& s){s.sendCommand(screen::Command::NormalDisplay);}, 500ms);
 }
 
 void ScreenTester::testRemapColorDepth() {
 
     screen::Color colors[screen::Geometry::Pixels];
 
-    for (Screen& screen : m_screens) {
-        screen.setColorDepth(screen::RemapColorDepth::Color256);
-        screen.applyRemapColorDepth();
-    }
+    broadcast([](Screen& s){s.setColorDepth(screen::RemapColorDepth::Color256); s.applyRemapColorDepth();});
 
     for (size_t i = 0; i < screen::Geometry::Pixels; i++) {
         colors[i].r = screen::ColorLimit::R_565_MAX;
@@ -39,15 +36,9 @@ void ScreenTester::testRemapColorDepth() {
 		colors[i].b = 0;
     }
 
-    for (Screen& screen : m_screens) {
-        screen.sendMultiPixel(colors, screen::Geometry::Pixels);
-    }
-    std::this_thread::sleep_for(std::chrono::milliseconds(500));
+    broadcast([&colors](Screen& s){s.sendMultiPixel(colors, screen::Geometry::Pixels);}, 500ms);
 
-    for (Screen& screen : m_screens) {
-        screen.setColorDepth(screen::RemapColorDepth::Color65k);
-        screen.applyRemapColorDepth();
-    }
+    broadcast([](Screen& s){s.setColorDepth(screen::RemapColorDepth::Color65k); s.applyRemapColorDepth();});
 
     for (size_t i = 0; i < screen::Geometry::Pixels; i++) {
         colors[i].r = 0;
@@ -55,58 +46,40 @@ void ScreenTester::testRemapColorDepth() {
 		colors[i].b = 0;
     }
 
-    for (Screen& screen : m_screens) {
-        screen.sendMultiPixel(colors, screen::Geometry::Pixels);
-    }
-    std::this_thread::sleep_for(std::chrono::milliseconds(500));
+    broadcast([&colors](Screen& s){s.sendMultiPixel(colors, screen::Geometry::Pixels);}, 500ms);
 
-    for (Screen& screen : m_screens) {
-        screen.setColorDepth(screen::RemapColorDepth::Color65kAlt);
-        screen.applyRemapColorDepth();
-    }
+    broadcast([](Screen& s){s.setColorDepth(screen::RemapColorDepth::Color65kAlt); s.applyRemapColorDepth();});
+
     for (size_t i = 0; i < screen::Geometry::Pixels; i++) {
         colors[i].r = 0;
 		colors[i].g = 0;
 		colors[i].b = screen::ColorLimit::B_565_MAX;
     }
 
-    for (Screen& screen : m_screens) {
-        screen.sendMultiPixel(colors, screen::Geometry::Pixels);
-    }
-    std::this_thread::sleep_for(std::chrono::milliseconds(500));
+    broadcast([&colors](Screen& s){s.sendMultiPixel(colors, screen::Geometry::Pixels);}, 500ms);
 
     std::random_device rd;
     std::mt19937 gen(rd());
     std::uniform_int_distribution<int> dist31(0, 31);
     std::uniform_int_distribution<int> dist63(0, 63);
 
-    for (Screen& screen : m_screens) {
-        screen.setColorDepth(screen::RemapColorDepth::Color256);
-        screen.applyRemapColorDepth();
-    }
+    broadcast([](Screen& s){s.setColorDepth(screen::RemapColorDepth::Color256); s.applyRemapColorDepth();});
+
     for (size_t i = 0; i < screen::Geometry::Pixels; i++) {
         colors[i].r = dist31(gen);
         colors[i].g = dist63(gen);
         colors[i].b = dist31(gen);
     }
 
-    for (Screen& screen : m_screens) {
-        screen.sendMultiPixel(colors, screen::Geometry::Pixels);
-    }
-    std::this_thread::sleep_for(std::chrono::milliseconds(500));
+    broadcast([&colors](Screen& s){s.sendMultiPixel(colors, screen::Geometry::Pixels);}, 500ms);
 
-        for (Screen& screen : m_screens) {
-        screen.setColorDepth(screen::RemapColorDepth::Color65k);
-        screen.applyRemapColorDepth();
-    }
+    broadcast([](Screen& s){s.setColorDepth(screen::RemapColorDepth::Color65k); s.applyRemapColorDepth();});
+
     for (size_t i = 0; i < screen::Geometry::Pixels; i++) {
         colors[i].r = dist31(gen);
         colors[i].g = dist63(gen);
         colors[i].b = dist31(gen);
     }
 
-    for (Screen& screen : m_screens) {
-        screen.sendMultiPixel(colors, screen::Geometry::Pixels);
-    }
-    std::this_thread::sleep_for(std::chrono::milliseconds(500));
+    broadcast([&colors](Screen& screen){screen.sendMultiPixel(colors, screen::Geometry::Pixels);}, 500ms);
 }
